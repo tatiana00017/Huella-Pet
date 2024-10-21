cc54c9596ec852c9a319bfd0adbdda4b2af8f4c4
tatiana00017 <tatis00017@gmail.com>
2024-10-20 21:31:00 -0500

    Se agrega alerta mis compras

--- misCompras/misCompras.js
+++ misCompras/misCompras.js
@@ -9,12 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 
                 window.location.href = misCompras.href;
             } else {
-                
-                alert("Debes iniciar sesión para acceder a Mis Compras.");
-                const loginButton = document.querySelector('.btnInicio-login');
-                if (loginButton) {
-                    loginButton.click();
-                }
+                return Swal.fire({
+                    icon: "warning",
+                    title: "Debe estar registrado",
+                    text: "Por favor, inicia sesión o regístrate para agregar productos al carrito.",
+                }).then(() => {
+                    const loginButton = document.querySelector('.btnInicio-login');
+                    if (loginButton) {
+                        loginButton.click(); 
+                    }
+                });
             }
         });
     }
